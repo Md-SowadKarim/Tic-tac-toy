29ecb11af674bf7fe129fbf72a3c88d1a991c466
Md. Sowad Karim <43478252+Md-SowadKarim@users.noreply.github.com>
2020-10-16 23:38:15 +0600

    Add files via upload
    First Commit

---
+++ WpfApplication1/Game.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Game
+    {
+
+        public string[] state = { "_", "_", "_", "_", "_", "_", "_", "_", "_" };
+        public string[,] nstate;
+        public char[,] cnstate;
+        public bool game_status = false;
+        public bool draw = false;
+        public Game()
+        {
+            nstate = new string[3, 3];
+            cnstate = new char[3, 3];
+
+        }
+
+        public void updateStates(string[] newstate)
+        {
+            state = newstate;
+
+        }
+        public void gameIsOn()
+        {
+            this.game_status = true;
+        }
+
+        public bool playerSelected(string sel_player)
+        {
+            if (sel_player.Equals("_"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void tochararray()
+        {
+           for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    cnstate[i, j] = char.Parse(nstate[i, j]);
+                }
+            }
+        }
+        public void predict(string cur_player)
+        {
+            convertState();
+            tochararray();
+
+            Move bestMove = new Move();
+            GFG gfg = new GFG();
+            bestMove = gfg.findBestMove(cnstate);
+            if (bestMove.row == -1 && bestMove.col == -1)
+            {
+                draw = true;
+            }
+            else
+            {
+                nstate[bestMove.row, bestMove.col] = cur_player;
+              
+            }
+
+            convertnState();
+
+            
+        }
+
+        public bool isDraw()
+        {
+            return draw;
+        }
+
+        public void convertnState()
+        {
+            int k = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    state[k] = nstate[i, j];
+                    k++;
+                }
+            }
+        }
+        public void convertState()
+        {
+            int k = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    nstate[i, j] = state[k];
+                    k++;
+                }
+
+            }
+        }
+
+
+        public bool hasMovesLeft()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if(nstate[i, j].Equals("_"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public string evluate()
+        {
+            //checking rows for victory
+            convertState();
+            for (int row = 0; row < 3; row++)
+            {
+                if (nstate[row, 0] == nstate[row, 1] && nstate[row, 1] == nstate[row, 2])
+                {
+                    if (nstate[row, 0].Equals("x"))
+                    {
+                        return "x";
+                    }
+                    else if (nstate[row, 0].Equals("o"))
+                    {
+                        return "o";
+                    }
+                    else
+                    {
+                        return "draw";
+                    }
+                }
+            }
+
+            //checking for column for victory
+            for (int col = 0; col < 3; col++)
+            {
+                if (nstate[0, col] == nstate[1, col] &&
+                    nstate[1, col] == nstate[2, col])
+                {
+                    if (nstate[0, col].Equals("x"))
+                    {
+                        return "x";
+                    }
+
+                    else if (nstate[0, col].Equals("o"))
+                    {
+                        return "o";
+                    }
+                }
+            }
+
+            if (nstate[0, 0] == nstate[1, 1] && nstate[1, 1] == nstate[2, 2])
+            {
+                if (nstate[0, 0] == "x")
+                {
+                    return "x";
+                }
+                else if (nstate[0, 0] == "o")
+                {
+                    return "o";
+                }
+            }
+
+            if (nstate[0, 2] == nstate[1, 1] && nstate[1, 1] == nstate[2, 0])
+            {
+                if (nstate[0, 2] == "x")
+                {
+                    return "x";
+                }
+                else if (nstate[0, 2] == "o")
+                {
+                    return "o";
+                }
+            }
+
+            return "_";
+
+        }
+
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string sel_player = "_";
+        public static string turn = "_";
+        public string[] game_state = { "b", "l", "a", "h", "x", "_", "_", "_", "_" };
+        public bool game_status = false;
+        public string cur_player = null;
+        Game newGame;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Tic Tac Toe with Ai";
+            newGame = new Game();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[0].Equals("_"))
+                {
+                    game_state[0] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    iswonordraw();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void x_checked(object sender, RoutedEventArgs e)
+        {
+            sel_player = "x";
+            turn = sel_player;
+            selected_player.Content = "Selected Player: " + sel_player;
+        }
+
+
+
+        private void o_checked(object sender, RoutedEventArgs e)
+        {
+            sel_player = "o";
+            turn = sel_player;
+            selected_player.Content = "Selected Player: " + sel_player;
+        }
+
+
+        private void playgame(object sender, RoutedEventArgs e)
+        {
+            rendergame();
+            
+            cur_player = sel_player;
+            current_player.Content = cur_player;
+            newGame.gameIsOn();
+
+            game_status = newGame.game_status;
+
+        }
+
+        private void rendergame()
+        {
+            game_state = newGame.state;
+            button1.Content = game_state[0];
+            button2.Content = game_state[1];
+            button3.Content = game_state[2];
+            button4.Content = game_state[3];
+            button5.Content = game_state[4];
+            button6.Content = game_state[5];
+            button7.Content = game_state[6];
+            button8.Content = game_state[7];
+            button9.Content = game_state[8];
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[1].Equals("_"))
+                {
+                    game_state[1] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    iswonordraw();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+
+        }
+
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[2].Equals("_"))
+                {
+                    game_state[2] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    iswonordraw();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void changeTurn()
+        {
+            if (turn == "o")
+            {
+                turn = "x";
+                current_player.Content = turn;
+                cur_player = turn;
+            }
+            else
+            {
+                turn = "o";
+                current_player.Content = turn;
+                cur_player = turn;
+            }
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[3].Equals("_"))
+                {
+                    game_state[3] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    iswonordraw();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[4].Equals("_"))
+                {
+                    game_state[4] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    iswonordraw();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[5].Equals("_"))
+                {
+                    game_state[5] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    string winstate = newGame.evluate();
+                    iswonordraw();
+                }
+                }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[6].Equals("_"))
+                {
+                    game_state[6] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    iswonordraw();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[7].Equals("_"))
+                {
+                    game_state[7] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            if (newGame.playerSelected(sel_player) && turn == sel_player)
+            {
+                if (game_state[8].Equals("_"))
+                {
+                    game_state[8] = sel_player;
+                    newGame.updateStates(game_state);
+                    rendergame();
+                    changeTurn();
+                    oplayer();
+                   
+                    iswonordraw();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Player Not Selected! or may be computer's turn");
+            }
+        }
+
+        public void oplayer() {
+            
+            newGame.predict(cur_player);
+            rendergame();
+            changeTurn();
+        }
+
+        public void iswonordraw()
+        {
+            int counter = 0;
+            if (newGame.isDraw())
+            {
+                won.Content = "Game is Draw";
+                rtgame();
+            }
+            else
+            {
+                string winstate = newGame.evluate();
+                if (winstate.Equals("_"))
+                {
+                    counter++;
+                }
+                else
+                {
+                    if (winstate.Equals("x"))
+                    {
+                        won.Content = "Winner is x";
+                        rtgame();
+                    }
+                    else if (winstate.Equals("o"))
+                    {
+                        won.Content = "Winner is o";
+                        rtgame();
+                    }
+                }
+            }
+        }
+
+        private void resetgame(object sender, RoutedEventArgs e)
+        {
+            rtgame();
+        }
+
+        public void rtgame()
+        {
+            Thread.Sleep(1000);
+            newGame = new Game();
+            rendergame();
+        }
+    }
+}
